356c460063fbd998b35b52b6c8b390b12c019806
JohannesMerz <johannes_merz@gmx.de>
2019-07-02 14:23:41 +0200

    fix lint

--- packages/cli/src/utils/getWebpackConfig.ts
+++ packages/cli/src/utils/getWebpackConfig.ts
@@ -54,7 +54,7 @@ export function getWebpackConfigPath(executionPath) {
     returnIfExists(
       `${executionPath}/node_modules/react-scripts/config/webpack.config.js`,
       () => {
-        return undefined
+        return undefined;
       }
     )
   );
