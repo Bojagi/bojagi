da8ae1bede604d06aef2ae88e532249ab07b45d8
Simon Jentsch <simon@bojagi.io>
2019-10-17 01:54:26 +0200

    feat(cli): Add CI environment default configs (#39)

--- packages/cli/src/utils/withDefaultArguments.ts
+++ packages/cli/src/utils/withDefaultArguments.ts
@@ -1,7 +1,10 @@
 import defaultConfig from '../defaultConfig';
+import getCiSettingsFactory from './getCiSettings';
+
+const getCiSettings = getCiSettingsFactory(process.env);
 
 const withDefaultArguments = action => args => {
-  const defaultedArgs = Object.assign({}, defaultConfig, args);
+  const defaultedArgs = Object.assign({}, getCiSettings(), defaultConfig, args);
   return action(defaultedArgs);
 };
 
