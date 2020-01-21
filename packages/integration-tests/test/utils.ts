385d4a916d28c387a3191c92192f47ad34607152
JohannesMerz <johannes_merz@gmx.de>
2020-01-21 23:21:57 +0100

    test: first version of integration tests

--- packages/integration-tests/test/utils.ts
+++ packages/integration-tests/test/utils.ts
@@ -3,41 +3,51 @@ import * as path from 'path';
 import * as fs from 'fs';
 import * as crypto from 'crypto';
 
-const EXEC_PATH = path.resolve(__dirname, '..');
-const BOJAGI_BIN = path.resolve(EXEC_PATH, 'node_modules', '.bin', 'bojagi');
-const RESULT_FOLDER = path.resolve(EXEC_PATH, '.bojagi', 'tmp');
+export const EXEC_PATH = path.resolve(__dirname, '..');
+export const BOJAGI_BIN = path.resolve(EXEC_PATH, 'node_modules', '.bin', 'bojagi');
+export const RESULT_FOLDER = path.resolve(EXEC_PATH, '.bojagi', 'tmp');
 
-function hash(aString) {
+export function deleteTmpFolder() {
+  fs.rmdirSync(RESULT_FOLDER, { recursive: true });
+}
+
+export function hash(aString) {
   const h = crypto.createHash('sha256');
   h.update(aString);
   return h.digest('hex');
 }
 
-function execBojagi(cmd) {
+export function execBojagi(cmd) {
   return execSync(`${BOJAGI_BIN} ${cmd}`, {
     cwd: EXEC_PATH,
   }).toString();
 }
 
-function snapShotFileJSON(filePath) {
+export function snapShotFileJSON(filePath) {
   expect(
     JSON.stringify(JSON.parse(fs.readFileSync(filePath, 'utf8')), null, ' ')
   ).toMatchSnapshot();
 }
 
-function snapShotFileHash(filePath) {
+export function snapShotFileHash(filePath) {
   expect(hash(fs.readFileSync(filePath, 'utf8'))).toMatchSnapshot();
 }
 
-test('bundle all components', () => {
-  execBojagi('bundle');
+export function snapShotTmpFolder() {
+  // TODO read all the files in dynamically, snapshot jsons, hash snapshots js files
+
   snapShotFileJSON(path.resolve(RESULT_FOLDER, 'files.json'));
   snapShotFileJSON(path.resolve(RESULT_FOLDER, 'components.json'));
 
   snapShotFileHash(path.resolve(RESULT_FOLDER, 'files', 'commons.js'));
+
   snapShotFileHash(
     path.resolve(RESULT_FOLDER, 'components', 'src__components__Box.js___default', 'component.js')
   );
+  snapShotFileJSON(
+    path.resolve(RESULT_FOLDER, 'components', 'src__components__Box.js___default', 'props.json')
+  );
+
   snapShotFileHash(
     path.resolve(
       RESULT_FOLDER,
@@ -46,6 +56,15 @@ test('bundle all components', () => {
       'component.js'
     )
   );
+  snapShotFileJSON(
+    path.resolve(
+      RESULT_FOLDER,
+      'components',
+      'src__components__BoxWithButtons.js___default',
+      'props.json'
+    )
+  );
+
   snapShotFileHash(
     path.resolve(
       RESULT_FOLDER,
@@ -54,4 +73,7 @@ test('bundle all components', () => {
       'component.js'
     )
   );
-});
+  snapShotFileJSON(
+    path.resolve(RESULT_FOLDER, 'components', 'src__components__Button.js___default', 'props.json')
+  );
+}
