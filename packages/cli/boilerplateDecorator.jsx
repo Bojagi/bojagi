e247262f79fefc4b0664ac889da6e2d16d28956a
Simon Jentsch <simon.jentsch@deliveryhero.com>
2020-07-21 00:53:17 +0200

    fix(cli): Fix typo in boilerplate decorator

--- packages/cli/boilerplateDecorator.jsx
+++ packages/cli/boilerplateDecorator.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Bojagi Decorators work like Storybook decorators.
-// With them uou can wrap your stories inside other components.
+// With them you can wrap your stories inside other components.
 // The global decorator is especially helpful to wrap context providers around your stories.
 
 // If you want to know more about decorators, you can read about them in the docs:
