9262c1549c4ab1ff2bf2ffc08603eacb338d1dd2
JohannesMerz <johannes_merz@gmx.de>
2020-10-27 20:46:49 +0100

    make gitpath optional if no git is installed

--- packages/cli/src/utils/getGitPath.ts
+++ packages/cli/src/utils/getGitPath.ts
@@ -2,13 +2,17 @@ import { execSync } from 'child_process';
 
 export default function getGitPath(filePath) {
   if (filePath) {
-    const result = execSync(`git ls-files --full-name ${filePath}`, {
-      env: process.env,
-      cwd: process.cwd(),
-    })
-      .toString()
-      .replace('\n', '');
-    return result || undefined;
+    try {
+      const result = execSync(`git ls-files --full-name ${filePath}`, {
+        env: process.env,
+        cwd: process.cwd(),
+      })
+        .toString()
+        .replace('\n', '');
+      return result || undefined;
+    } catch (e) {
+      return undefined;
+    }
   }
   return undefined;
 }
