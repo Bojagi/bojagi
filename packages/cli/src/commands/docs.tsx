80663d65cc8dddc41969cb95fcc36ee0bad10e59
Simon Jentsch <simon@bojagi.io>
2020-11-11 00:17:04 +0100

    chore(cli): remove some dependency warnings on installation

--- packages/cli/src/commands/docs.tsx
+++ packages/cli/src/commands/docs.tsx
@@ -1,10 +1,10 @@
-import opn from 'opn';
+import open = require('open');
 
 export default function docs(program) {
   program
     .command('docs')
     .description('Open the Bojagi documentation in the browser')
     .action(() => {
-      opn('https://bojagi.io/docs');
+      open('https://bojagi.io/docs');
     });
 }
