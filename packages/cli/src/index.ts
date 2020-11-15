7e68aa3908ccadd86dc06e645eb1ae694e81aa58
Simon Jentsch <simon@bojagi.io>
2020-11-15 15:22:45 +0100

    Move format reset to the end

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -24,8 +24,8 @@ const FORMAT_RESET = '\x1b[0m';
 
 const [nodeMajorVersion] = process.versions.node.split('.');
 if (parseInt(nodeMajorVersion, 10) < MINIMUM_NODE_VERSION) {
-  console.error(`${FORMAT_UNDERLINE}${COLOR_RED}You have an outdated node version!`);
-  console.error(`${FORMAT_RESET}${COLOR_YELLOW}Your version: ${process.versions.node}`);
+  console.error(`${FORMAT_UNDERLINE}${COLOR_RED}You have an outdated node version!${FORMAT_RESET}`);
+  console.error(`${COLOR_YELLOW}Your version: ${process.versions.node}`);
   console.error(`Minimum required version: ${MINIMUM_NODE_VERSION}`);
   console.error('Please update your node environment!');
   process.exit(1);
