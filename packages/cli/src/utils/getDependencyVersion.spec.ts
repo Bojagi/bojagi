9c49dd1bc1c1f0c6e3fa650d2ff7b051ef17a7e4
Simon Jentsch <simon@bojagi.io>
2021-01-22 23:47:27 +0100

    Search in execution path for react version

--- packages/cli/src/utils/getDependencyVersion.spec.ts
+++ packages/cli/src/utils/getDependencyVersion.spec.ts
@@ -1,17 +1,27 @@
 import { getDependencyVersion } from './getDependencyVersion';
 
 test("Return undefined dependency wasn't found", () => {
-  const reqMock = jest.fn(() => {
+  const reqMock = createReqMock(() => {
     throw new Error();
   });
-  const result = getDependencyVersion('react', reqMock as any);
+  const result = getDependencyVersion('react', 'something', reqMock as any);
   expect(result).toBeUndefined();
 });
 
 test('Find dependency version when package exists', () => {
-  const reqMock = jest.fn(() => ({
+  const reqMock = createReqMock(() => ({
     version: '16.13.1',
   }));
-  const result = getDependencyVersion('react', reqMock as any);
+  const result = getDependencyVersion('react', '/some/folder', reqMock as any);
   expect(result).toBe('16.13.1');
+  expect(reqMock.resolve).toHaveBeenCalledWith('react/package.json', {
+    paths: ['/some/folder', '/some'],
+  });
+  expect(reqMock).toHaveBeenCalledWith('some/thing');
 });
+
+function createReqMock(cb) {
+  const reqMock: any = jest.fn(cb);
+  reqMock.resolve = jest.fn(() => 'some/thing');
+  return reqMock;
+}
--- packages/cli/src/utils/getDependencyVersion.ts
+++ packages/cli/src/utils/getDependencyVersion.ts
@@ -1,7 +1,17 @@
-export function getDependencyVersion(dependencyName: string, req = require) {
+import * as path from 'path';
+
+export function getDependencyVersion(dependencyName: string, searchPath: string, req = require) {
   try {
-    return req(`${dependencyName}/package.json`).version;
+    const resolvedPath = req.resolve(`${dependencyName}/package.json`, {
+      paths: getAllParentFolders(searchPath),
+    });
+    return req(resolvedPath).version;
   } catch {
     return undefined;
   }
+}
+
+function getAllParentFolders(fullPath: string) {
+  const segments = fullPath.split(path.sep).filter(item => item);
+  return segments.map((_, i, arr) => path.sep + path.join(...arr.slice(0, i + 1))).reverse();
 }
