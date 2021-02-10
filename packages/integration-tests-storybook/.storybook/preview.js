4d191a6236b6e6a32e71c5bd9cc621506953c562
Simon Jentsch <simon@bojagi.io>
2021-02-10 15:48:10 +0100

    feat(cli): Add support for SB globals in context

--- packages/integration-tests-storybook/.storybook/preview.js
+++ packages/integration-tests-storybook/.storybook/preview.js
@@ -2,3 +2,16 @@
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export const decorators = [(Story, {globals, args}) => {
+  return <div {...globals}><Story /></div>;
+}];
+
+export const globalTypes = {
+  hans: {
+    defaultValue: 'wurst',
+  },
+  hansi: {
+    defaultValue: 'würstchen',
+  },
+}
