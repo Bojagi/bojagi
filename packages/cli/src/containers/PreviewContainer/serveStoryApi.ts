0a4896016c6827e9142f946a44a6caa1bd40fc35
Simon Jentsch <simon@bojagi.io>
2021-02-23 12:00:14 +0100

    feat(cli): Add story ordering for preview mode (#163)

--- packages/cli/src/containers/PreviewContainer/serveStoryApi.ts
+++ packages/cli/src/containers/PreviewContainer/serveStoryApi.ts
@@ -1,3 +1,4 @@
+import { StoryCollectionMetadata } from '../../steps/analyze';
 import { SetupApiOptions } from './setupApi';
 
 export function serveStoriesApi({ storiesMetadata, getFiles, getAssets }: SetupApiOptions) {
@@ -5,13 +6,27 @@ export function serveStoriesApi({ storiesMetadata, getFiles, getAssets }: SetupA
 
   return {
     files: getFiles().map(({ url, name }) => ({ name, url })),
-    stories: Object.entries(storiesMetadata).map(([filePath, meta]) => {
-      return {
-        files: assets[meta.fileName],
-        storyItems: meta.storyItems,
-        filePath,
-        title: meta.title,
-      };
-    }),
+    stories: Object.entries(storiesMetadata)
+      .sort(sortStory)
+      .map(([filePath, meta]) => {
+        return {
+          files: assets[meta.fileName],
+          storyItems: meta.storyItems,
+          filePath,
+          title: meta.title,
+        };
+      }),
   };
+}
+
+function sortStory(a: [string, StoryCollectionMetadata], b: [string, StoryCollectionMetadata]) {
+  if (a[1].title < b[1].title) {
+    return -1;
+  }
+
+  if (a[1].title > b[1].title) {
+    return 1;
+  }
+
+  return 0;
 }
