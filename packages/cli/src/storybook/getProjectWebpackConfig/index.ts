2c4a6e1f22cd60f61a56897ed09cecd5fd093550
JohannesMerz <johannes_merz@gmx.de>
2021-02-23 17:16:06 +0100

    fix in semver usage

--- packages/cli/src/storybook/getProjectWebpackConfig/index.ts
+++ packages/cli/src/storybook/getProjectWebpackConfig/index.ts
@@ -4,14 +4,21 @@ import * as semver from 'semver';
 import { getStorybookFramework, getStorybookVersion } from '../storybookUtils';
 import { getV_6_1_X_StorybookProjectWebpackConfig } from './v6.1.x';
 import { getV_6_2_X_StorybookProjectWebpackConfig } from './v6.2.x';
+import debuggers, { DebugNamespaces } from '../../debug';
+
+const debug = debuggers[DebugNamespaces.STORYBOOK];
 
 export async function getStorybookProjectWebpackConfig(): Promise<webpack.Configuration | void> {
   const framework = getStorybookFramework();
   if (framework) {
+    debug(`storybook framework detected: ${framework}`);
     const version = getStorybookVersion(framework);
-    if (semver.gte(version, '6.2.0')) {
+    debug(`storybook version: ${version}`);
+    if (semver.gte(semver.coerce(version), '6.2.0')) {
+      debug('version greater than 6.2.0, using V_6_2_X configuration');
       return getV_6_2_X_StorybookProjectWebpackConfig(framework);
     }
+    debug('version below 6.2.0, using V_6_1_X configuration');
     return getV_6_1_X_StorybookProjectWebpackConfig(framework);
   }
   return undefined;
