30bcf31773f2f9e1a084341997719047764a54ed
Simon Jentsch <simon@bojagi.io>
2021-05-11 11:23:48 +0200

    Refactor copyStaticFiles

--- packages/cli/src/utils/copyStaticFiles.ts
+++ packages/cli/src/utils/copyStaticFiles.ts
@@ -3,29 +3,27 @@ import { getFS } from '../dependencies';
 
 const fs = getFS();
 
-export async function copyStaticFiles(
-  folders: string[],
-  outputPath: string,
-  subPath: string = ''
-): Promise<string[]> {
+async function copyStaticFilesFromFolder(folder: string, outputPath: string, subPath: string = '') {
+  const folderFiles = await fs.promises.readdir(path.join(folder, subPath));
   return Promise.all(
-    folders.filter(fs.existsSync).map(async folder => {
-      const folderFiles = await fs.promises.readdir(path.join(folder, subPath));
-      return Promise.all(
-        folderFiles.map(async folderFile => {
-          const relativePath = path.join(subPath, folderFile);
-          const fullPath = path.join(folder, relativePath);
-          const fullOutputPath = path.join(outputPath, relativePath);
-          const stats = await fs.promises.stat(fullPath);
+    folderFiles.map(async folderFile => {
+      const relativePath = path.join(subPath, folderFile);
+      const fullPath = path.join(folder, relativePath);
+      const fullOutputPath = path.join(outputPath, relativePath);
+      const stats = await fs.promises.stat(fullPath);
 
-          if (stats.isDirectory()) {
-            await fs.promises.mkdir(fullOutputPath);
-            return copyStaticFiles([folder], outputPath, relativePath);
-          }
-          await fs.promises.copyFile(fullPath, fullOutputPath);
-          return [relativePath];
-        })
-      ).then(files => files.flat());
+      if (stats.isDirectory()) {
+        await fs.promises.mkdir(fullOutputPath);
+        return copyStaticFilesFromFolder(folder, outputPath, relativePath);
+      }
+      await fs.promises.copyFile(fullPath, fullOutputPath);
+      return [relativePath];
     })
+  ).then(files => files.flat());
+}
+
+export async function copyStaticFiles(folders: string[], outputPath: string): Promise<string[]> {
+  return Promise.all(
+    folders.filter(fs.existsSync).map(folder => copyStaticFilesFromFolder(folder, outputPath))
   ).then(files => files.flat());
 }
